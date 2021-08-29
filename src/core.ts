7ee5008772d1aa1101c60d3a7060b32072cb2ddc
Matt Schrage <matt@fig.io>
2021-08-29 15:43:35 -0700

    Update file structure

---
+++ src/core.ts
@@ -0,0 +1,84 @@
+import {
+  ServerOriginatedMessage,
+  ClientOriginatedMessage,
+} from "./fig";
+
+import { b64ToBytes, bytesToBase64 } from "./utils";
+
+interface GlobalAPIError {
+  error: string;
+}
+
+const FigGlobalErrorOccurred = "FigGlobalErrorOccurred";
+const FigProtoMessageRecieved = "FigProtoMessageRecieved";
+
+type shouldKeepListening = boolean;
+
+type APIResponseHandler = (
+  response: ServerOriginatedMessage["submessage"]
+) => shouldKeepListening | void;
+
+let messageId = 0;
+const handlers: Record<number, APIResponseHandler> = {};
+
+export const sendMessage = (
+  message: ClientOriginatedMessage["submessage"],
+  handler?: APIResponseHandler
+): void => {
+  const request: ClientOriginatedMessage = {
+    id: (messageId += 1),
+    submessage: message,
+  };
+
+  if (handler && request.id) {
+    handlers[request.id] = handler;
+  }
+
+  const buffer = ClientOriginatedMessage.encode(request).finish();
+  const b64 = bytesToBase64(buffer);
+  console.log(b64);
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  if (window.webkit.messageHandlers.proto == null) {
+    console.error(
+      "This version of Fig does not support using protocol buffers. Please update."
+    );
+  }
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  window.webkit.messageHandlers.proto.postMessage(b64);
+};
+
+const recievedMessage = (response: ServerOriginatedMessage): void => {
+  if (response.id === undefined) {
+    return;
+  }
+
+  const keepListeningOnID = handlers[response.id](response.submessage);
+
+  if (!keepListeningOnID) {
+    delete handlers[response.id];
+  }
+};
+
+const setupEventListeners = (): void => {
+  document.addEventListener(FigGlobalErrorOccurred, (event: Event) => {
+    const response = (<CustomEvent>event).detail as GlobalAPIError;
+    console.error(response.error);
+  });
+
+  document.addEventListener(FigProtoMessageRecieved, (event: Event) => {
+    const raw = (<CustomEvent>event).detail as string;
+
+    const bytes = b64ToBytes(raw);
+
+    const message = ServerOriginatedMessage.decode(bytes);
+
+    recievedMessage(message);
+  });
+};
+
+// We want this to be run automatically
+console.log("Setting up Fig event listeners...");
+setupEventListeners();
---
+++ src/utils.ts
@@ -0,0 +1,122 @@
+/* eslint-disable no-bitwise */
+export const b64EncodeUnicode = (str: string): string =>
+  // first we use encodeURIComponent to get percent-encoded UTF-8,
+  // then we convert the percent encodings into raw bytes which
+  // can be fed into btoa.
+  btoa(
+    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (_, p1) =>
+      String.fromCharCode(Number(`0x${p1}`))
+    )
+  );
+
+export const b64DecodeUnicode = (str: string): string =>
+  // Going backwards: from bytestream, to percent-encoding, to original string.
+  decodeURIComponent(
+    atob(str)
+      .split("")
+      .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
+      .join("")
+  );
+
+export const b64ToBytes = (base64: string): Uint8Array => {
+  const binaryString = window.atob(base64);
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i += 1) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes;
+};
+
+const base64abc = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "+",
+  "/",
+];
+
+export function bytesToBase64(bytes: Uint8Array): string {
+  let result = "";
+  let i;
+  const l = bytes.length;
+  for (i = 2; i < l; i += 3) {
+    result += base64abc[bytes[i - 2] >> 2];
+    result += base64abc[((bytes[i - 2] & 0x03) << 4) | (bytes[i - 1] >> 4)];
+    result += base64abc[((bytes[i - 1] & 0x0f) << 2) | (bytes[i] >> 6)];
+    result += base64abc[bytes[i] & 0x3f];
+  }
+  if (i === l + 1) {
+    // 1 octet yet to write
+    result += base64abc[bytes[i - 2] >> 2];
+    result += base64abc[(bytes[i - 2] & 0x03) << 4];
+    result += "==";
+  }
+  if (i === l) {
+    // 2 octets yet to write
+    result += base64abc[bytes[i - 2] >> 2];
+    result += base64abc[((bytes[i - 2] & 0x03) << 4) | (bytes[i - 1] >> 4)];
+    result += base64abc[(bytes[i - 1] & 0x0f) << 2];
+    result += "=";
+  }
+  return result;
+}
