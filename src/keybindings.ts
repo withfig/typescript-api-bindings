18561b475aa814d7b1c3c47a476abddf2c1ab1ad
Matt Schrage <matt@fig.io>
2021-09-29 11:24:03 -0700

    Ensure keybindings return Subscription object

--- src/keybindings.ts
+++ src/keybindings.ts
@@ -3,7 +3,7 @@ import { _subscribe } from "./notifications";
 
 
 const subscribe = (handler: (notification: KeybindingPressedNotification) => boolean | undefined) => {
-    _subscribe({ type: NotificationType.NOTIFY_ON_KEYBINDING_PRESSED }, (notification) => {
+    return _subscribe({ type: NotificationType.NOTIFY_ON_KEYBINDING_PRESSED }, (notification) => {
         switch (notification?.type?.$case) {
             case "keybindingPressedNotification":
                 return handler(notification.type.keybindingPressedNotification)
