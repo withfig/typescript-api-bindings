f91ec1d2451a258f1d33c9305a9a395480fe6a9d
Matt Schrage <matt@fig.io>
2021-10-15 17:28:17 -0700

    tweak exports

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ import PTY from './pty'
 import Shell from './shell'
 import Keybindings from './keybindings'
 
-// @ts-ignore
-window.f =  { WindowPosition, Settings, EditBufferNotifications, PTY, Shell, Keybindings}
+import * as Fig from "./fig";
+import * as Internal from "./requests"
 
-export * from "./fig";
-export * from "./requests"
-export { WindowPosition, Settings, EditBufferNotifications, PTY, Shell, Keybindings}
+// @ts-ignore
+window.f =  { WindowPosition, Settings, EditBufferNotifications, PTY, Shell, Keybindings, Internal}
+
+export { WindowPosition, Settings, EditBufferNotifications, PTY, Shell, Keybindings, Internal, Fig}
